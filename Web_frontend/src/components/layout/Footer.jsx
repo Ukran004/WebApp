b716e9902d5a1feec686ae47e5475712a37f50a3
Ukran004 <110590995+Ukran004@users.noreply.github.com>
2024-03-01 20:58:42 +0545

    some changes

--- Web_frontend/src/components/layout/Footer.jsx
+++ Web_frontend/src/components/layout/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 			<Container>
 				<Row>
 					<Col xs={12} md={12} className="text-center">
-						<p className="mb-0"> &copy; {today.getFullYear()} Chandragiri Inn </p>
+						<p className="mb-0"> &copy; {today.getFullYear()} RoomRental</p>
 					</Col>
 				</Row>
 			</Container>
